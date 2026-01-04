c8686f34898d57457984747571d8a6573230b464
Lea Silakov <me@ileamare.ru>
2026-01-04 19:32:18 +0300

    scss build updates

--- scripts/css.js
+++ scripts/css.js
@@ -1,36 +1,24 @@
 require('dotenv').config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env' });
-const sass = require('node-sass');
+const sass = require('sass');
 const CleanCSS = require('clean-css');
 const git = require('git-rev-sync');
 const fs = require('fs');
 
-sass.render({ file: 'src/scss/app.scss' }, (err, renderedCss) => {
-    if (err) {
-        console.log(err);
+try {
+    const result = sass.compile('src/scss/app.scss');
+    console.log('Css compiled.');
+    let contents = result.css.toString();
+    let outFile = 'build/app.css';
+    fs.writeFileSync(outFile, contents, 'utf8');
+    console.log(`${outFile} was successfully saved.`);
+    
+    if (process.env.NODE_ENV === 'production') {
+        const minified = new CleanCSS({ rebase: false }).minify(contents).styles;
+        outFile = `build/app-${git.short()}.min.css`;
+        fs.writeFileSync(outFile, minified);
+        console.log(`${outFile} was successfully saved.`);
     }
-    else {
-        console.log('Css compiled.');
-        let contents = renderedCss.css;
-        let outFile = 'build/app.css';
-        fs.writeFile(outFile, contents, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(`${outFile} was successfully saved.`);
-                if (process.env.NODE_ENV === 'production') {
-                    const result = new CleanCSS({ rebase: false }).minify(contents).styles;
-                    outFile = `build/app-${git.short()}.min.css`;
-                    fs.writeFile(outFile, result, (err) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            console.log(`${outFile} was successfully saved.`);
-                        }
-                    });
-                }
-            }
-        });
-    }
-});
+} catch (err) {
+    console.error('Sass compilation error:', err);
+    process.exit(1);
+}
